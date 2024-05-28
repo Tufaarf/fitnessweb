4a64c3035f37402b88be097ed488e58a0ad48541
Tufaarf <daffa.tfail@gmail.com>
2024-05-28 11:31:48 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// toggle kelas active
+const navbarNav = document.querySelector('.navbar-nav');
+// if ham menu click
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// outside click sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+});
+// slider
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-slider' ).mount();
+  } );
